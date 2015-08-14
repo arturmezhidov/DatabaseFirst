45ea99525a34ba1ebe5cecaf3d01ba2cb21f7b97
Artur Mezhidov <artur.mezhidov@itechart-group.com>
2015-08-14 18:36:04 +0300

    ready

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var db = new CompanyContext())
+			{
+				var query = from employee in db.Employees
+							orderby employee.FirstName
+							select employee;
+
+				Console.WriteLine("All employees:");
+				foreach (var employee in query)
+				{
+					Console.WriteLine("{0}) {1} {2}.", employee.Id, employee.FirstName, employee.LastName);
+				}
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
